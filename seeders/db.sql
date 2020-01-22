52cd8e40a3ce26cf50929ffbe833c27fc206d9b5
Cristian Colocho <chosafine@icloud.com>
2020-01-22 13:29:47 -0500

    Initial Work For Database

--- seeders/db.sql
+++ seeders/db.sql
@@ -1,1 +1,15 @@
-// asldkjf
+DROP DATABASE IF EXISTS food_db;
+
+CREATE DATABASE food_db;
+
+USE food_db 
+
+CREATE TABLE products(
+    id INT,
+    product_name VARCHAR(500),
+    selling_price DEC(10,2), 
+	tax_exempt BOOL, 
+	tax_percentage INT, 
+	category VARCHAR(500), 
+	barcode VARCHAR(30)
+);
