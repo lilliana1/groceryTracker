17fef10eb838034af8e0b277bac52e0297e8a0e3
Cristian Colocho <chosafine@icloud.com>
2020-01-23 11:06:50 -0500

    refactoring

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_HOST,
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
@@ -10,7 +10,7 @@ module.exports = {
     "operatorsAliases": false
   },
   "test": {
-    "username": process.env.DB_HOST,
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
@@ -18,7 +18,7 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": process.env.DB_HOST,
+    "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
