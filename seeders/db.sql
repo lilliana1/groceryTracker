5a6756fe3106c9a774125a102a228365325b906b
Cristian Colocho <chosafine@icloud.com>
2020-01-23 11:42:07 -0500

    updating the database information to support img srcs

--- seeders/db.sql
+++ seeders/db.sql
@@ -1,15 +1,27 @@
 DROP DATABASE IF EXISTS food_db;
-
 CREATE DATABASE food_db;
-
-USE food_db 
-
-CREATE TABLE products(
-    id INT,
-    product_name VARCHAR(500),
-    selling_price DEC(10,2), 
-	tax_exempt BOOL, 
-	tax_percentage INT, 
-	category VARCHAR(500), 
-	barcode VARCHAR(30)
+USE food_db;
+CREATE TABLE Products(
+  id INT PRIMARY KEY,
+  product_name VARCHAR(500),
+  selling_price DEC(10, 2),
+  image_src VARCHAR(999),
+  tax_exempt BOOL,
+  tax_percentage INT,
+  category VARCHAR(500),
+  barcode VARCHAR(30)
+);
+CREATE TABLE Users(
+  id INT PRIMARY KEY,
+  first_name VARCHAR(500),
+  last_name VARCHAR(500),
+  email VARCHAR(500),
+  password VARCHAR(70)
+);
+CREATE TABLE Grocery_List(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  productId INT NOT NULL,
+  userId INT NOT NULL,
+  FOREIGN KEY (productId) REFERENCES Products(id),
+  FOREIGN KEY (userId) REFERENCES Users(id)
 );
