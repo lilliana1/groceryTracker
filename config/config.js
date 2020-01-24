b3932acf46515ab965e1c321ff19ddafa44457c6
Brandon Downhour <xtremeelement@gmail.com>
2020-01-24 11:46:21 -0500

    Fixed errors with quotes

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    "operatorsAliases": false
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    operatorsAliases: false
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    operatorsAliases: false
   },
-  “production”: {
-    "use_env_variable": “JAWSDB_URL”,
-    "dialect": “mysql”
-    }
-}
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
